14b3387d3d5d183c56f211940003e200c86f2de6
Victor Langat <provicml@gmail.com>
2024-01-24 01:58:40 +0300

    make executable

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -21,9 +21,8 @@ if __name__ == "__main__":
     print("{} logs".format(collection.count_documents({})))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for met in methods:
-        print("\tmethod {}: {}".format(
-            met, collection.count_documents({"method": met})))
+    for method in methods:
+        print("\tmethod {}: {}".format(method, collection.count_documents({"method": method})))
     print("{} status check".format(
         collection.count_documents({"path": "/status"})))
 
