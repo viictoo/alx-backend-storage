94370bb3f986b490895d822f42feaba3f81442b6
Victor Langat <provicml@gmail.com>
2024-01-24 01:57:00 +0300

    code fix fstring noe pep8

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -17,13 +17,15 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient('mongodb://localhost:27017')
     collection = client.logs.nginx
 
     print("{} logs".format(collection.count_documents({})))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
+        # print(collection.count_documents({"method": method}))
         print(
-            '\tmethod {method}: {collection.count_documents({"method", method})}')
-    print(f'{collection.count_documents({"path": "/status"})} status check')
+            f'\tmethod {method}: {collection.count_documents({"method": method})}'
+            )
+    print(f"{collection.count_documents({'path': '/status'})} status check")
